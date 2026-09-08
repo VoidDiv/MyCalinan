7cda79ed7991c7907558f742535dd6da45deb878
normanpalermo-png <norman.palermo@pctdavao.edu.ph>
2026-09-08 19:52:20 +0800

    Update firebase config with correct keys

--- lib/Firebase.ts
+++ lib/Firebase.ts
@@ -5,12 +5,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-apiKey: "AIzaSyBu_HalimbawaLangIto_M2y4", 
-authDomain: "mycalinan.firebaseapp.com",
-projectId: "mycalinan",
-storageBucket: "mycalinan.appspot.com",
-messagingSenderId: "123456789012",
-appId: "1:123456789012:web:abc123def456"
+  apiKey: "AIzaSyAp8AuPjfTaAZH51epl3ZtAPTkqWAbr6s0",
+  authDomain: "mycalinan.firebaseapp.com",
+  projectId: "mycalinan",
+  storageBucket: "mycalinan.firebasestorage.app",
+  messagingSenderId: "519302589088",
+  appId: "1:519302589088:web:432887b465fb8ceacbb7c5",
+  measurementId: "G-QQ3JJR882G"
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
